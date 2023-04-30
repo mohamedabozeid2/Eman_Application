48303205f71905fdda9baa6b42178642b42d11f0
Abo Zeid <hamoashraf1960@gmail.com>
2023-04-30 16:41:04 +0300

    remove access fine location permission

--- lib/layers/presentation/screens/splash_screen/splash_screen.dart
+++ lib/layers/presentation/screens/splash_screen/splash_screen.dart
@@ -3,10 +3,13 @@ import 'dart:async';
 import 'package:eman_application/core/utils/app_values.dart';
 import 'package:eman_application/core/utils/assets_manager.dart';
 import 'package:eman_application/core/utils/components.dart';
+import 'package:eman_application/layers/presentation/controller/main_cubit/main_status.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../controller/main_cubit/main_cubit.dart';
 import '../main_screen/main_screen.dart';
+import '../server_error_screen/server_error_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -29,24 +32,34 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(AppSize.s40),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Image(
-                  image: AssetImage(
-                    AssetsManager.appLogo,
-                  ),
-                )
-              ],
+    return BlocConsumer<MainCubit, MainStates>(
+      listener: (context, state) {
+        if (state is MainGetQuranServerErrorState) {
+          Components.navigateAndFinish(
+              context: context, widget: const ServerErrorScreen());
+        }
+      },
+      builder: (context, state) {
+        return Scaffold(
+          body: SafeArea(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(AppSize.s40),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Image(
+                      image: AssetImage(
+                        AssetsManager.appLogo,
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
